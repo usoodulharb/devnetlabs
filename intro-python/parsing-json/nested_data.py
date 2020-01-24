9951bc15c084b88864e90d576cdeacd1ab8bafaa
usoodulharb <shoaibh370@gmail.com>
2020-01-24 19:33:32 +0500

    complete nested data lab

--- intro-python/parsing-json/nested_data.py
+++ intro-python/parsing-json/nested_data.py
@@ -12,7 +12,15 @@ here = os.path.abspath(os.path.dirname(__file__))
 
 with open(os.path.join(here, "interfaces.json")) as file:
     # TODO: Parse the contents of the JSON file into a variable
+    json_cont = json.load(file)
 
 
 # TODO: Loop through the interfaces in the JSON data and print out each
 # interface's name, ip, and netmask.
+#for content in json_cont["ietf-interfaces:interfaces"]["interface"]:
+#    print(content["name"])
+
+for content in json_cont["ietf-interfaces:interfaces"]["interface"]:
+    for eyy in content["ietf-ip:ipv4"]["address"]:
+        print(content.get("name"), eyy.get("ip"), eyy.get("netmask"))
+
